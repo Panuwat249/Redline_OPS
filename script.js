e3096269a6c150f14e66229e244b80562e1e061a
KhaiDao <129141028+Panuwat249@users.noreply.github.com>
2026-07-02 15:45:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-fetch("data.json")
-.then(res => res.json())
-.then(data => {
-
-for(let key in data){
-let el = document.getElementById(key);
-
-if(el){
-if(typeof data[key] === "number"){
-if(key.includes("distance") || key.includes("trip")){
-el.innerText = data[key].toLocaleString();
-}else if(
-key.includes("ontime") ||
-key.includes("reliable") ||
-key.includes("ready")
-){
-el.innerText = data[key] + "%";
-}else{
-el.innerText = data[key];
-}
-}else{
-el.innerText = data[key];
-}
-}
-}
-
-});
+document.getElementById("date").innerText =
+  new Date().toLocaleDateString("th-TH", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
