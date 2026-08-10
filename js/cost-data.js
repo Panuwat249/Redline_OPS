7fc9caa003db664fdb41e8789d10e18612d82ccc
KhaiDao <129141028+Panuwat249@users.noreply.github.com>
2026-08-10 13:53:30 +0700

    Update cost-data.js

--- js/cost-data.js
+++ js/cost-data.js
@@ -2,37 +2,37 @@ window.costData = [
     {
         fiscalYear: "2565",
 
-        staffCost: 0,
-        energyCost: 0,
-        maintenanceCost: 0,
-        indirectCost: 0
+        staffCost: 305158077.31,
+        energyCost: 127269813.25,
+        maintenanceCost: 82593098.90,
+        indirectCost: 229603946.04
     },
 
     {
         fiscalYear: "2566",
 
-        staffCost: 0,
-        energyCost: 0,
-        maintenanceCost: 0,
-        indirectCost: 0
+        staffCost: 319543472.27,
+        energyCost: 202530078.04,
+        maintenanceCost: 187437417.42,
+        indirectCost: 248834004.86
     },
 
     {
         fiscalYear: "2567",
 
-        staffCost: 0,
-        energyCost: 0,
-        maintenanceCost: 0,
-        indirectCost: 0
+        staffCost: 353331362.11,
+        energyCost: 203848122.70,
+        maintenanceCost: 219525131.41,
+        indirectCost: 263227852.49
     },
 
     {
         fiscalYear: "2568",
 
-        staffCost: 0,
-        energyCost: 0,
-        maintenanceCost: 0,
-        indirectCost: 0
+        staffCost: 377136200.26,
+        energyCost: 186154879.23,
+        maintenanceCost: 249696406.18,
+        indirectCost: 128964882.12
     },
 
     {
