61ad45553d986d964587325a1e970fbbe02676a8
KhaiDao <129141028+Panuwat249@users.noreply.github.com>
2026-08-10 12:30:59 +0700

    Create cost-data.js

---
+++ js/cost-data.js
@@ -0,0 +1,46 @@
+window.costData = [
+    {
+        fiscalYear: "2565",
+
+        staffCost: 0,
+        energyCost: 0,
+        maintenanceCost: 0,
+        indirectCost: 0
+    },
+
+    {
+        fiscalYear: "2566",
+
+        staffCost: 0,
+        energyCost: 0,
+        maintenanceCost: 0,
+        indirectCost: 0
+    },
+
+    {
+        fiscalYear: "2567",
+
+        staffCost: 0,
+        energyCost: 0,
+        maintenanceCost: 0,
+        indirectCost: 0
+    },
+
+    {
+        fiscalYear: "2568",
+
+        staffCost: 0,
+        energyCost: 0,
+        maintenanceCost: 0,
+        indirectCost: 0
+    },
+
+    {
+        fiscalYear: "2569",
+
+        staffCost: 0,
+        energyCost: 0,
+        maintenanceCost: 0,
+        indirectCost: 0
+    }
+];
